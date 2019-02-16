e258aea4df3848d353318d47fe17c283e45e092c
Vishal-Dash <vishaldash1998@gmail.com>
2019-02-16 16:55:58 +0530

    Update config_paytm.php

--- lib/config_paytm.php
+++ lib/config_paytm.php
@@ -7,10 +7,10 @@
 - Above details will be different for testing and production environment.
 */
 
-define('PAYTM_ENVIRONMENT', 'PROD'); // PROD
-define('PAYTM_MERCHANT_KEY', 'ASm2Z6Ub5DygJIbf!u@!32i0IUZ9Kj%I'); //Change this constant's value with Merchant key received from Paytm.
-define('PAYTM_MERCHANT_MID', 'bHtGWd43877257702694'); //Change this constant's value with MID (Merchant ID) received from Paytm.
-define('PAYTM_MERCHANT_WEBSITE', 'DEFAULT'); //Change this constant's value with Website name received from Paytm.
+define('PAYTM_ENVIRONMENT', 'TEST'); // PROD
+define('PAYTM_MERCHANT_KEY', 'DZJ32h6TgIg&1rjW'); //Change this constant's value with Merchant key received from Paytm.
+define('PAYTM_MERCHANT_MID', 'WXlTuR86831744936162'); //Change this constant's value with MID (Merchant ID) received from Paytm.
+define('PAYTM_MERCHANT_WEBSITE', 'WEB_STAGING'); //Change this constant's value with Website name received from Paytm.
 
 $PAYTM_STATUS_QUERY_NEW_URL='https://securegw-stage.paytm.in/merchant-status/getTxnStatus';
 $PAYTM_TXN_URL='https://securegw-stage.paytm.in/theia/processTransaction';
